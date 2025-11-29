e9c5f3ee370567035475e47a38166765af2a1d0c
Bachar hassan <bacharhassan@Bachars-MacBook-Pro.local>
2025-11-30 01:27:21 +0200

    Add database.sql

---
+++ project/database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title TEXT NOT NULL,
+    completed TINYINT(1) DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
